edd1456241bf1bd599feba1c3664d8ebcf8594db
Aarush2907 <aarush.taluja1@gmail.com>
2025-12-31 05:19:41 +0530

    chore: initialize React workflow builder

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,10 @@
+import "../styles/canvas.css";
+
+export default function Canvas() {
+  return (
+    <div className="canvas">
+      <h1>Workflow Builder</h1>
+      <p>Start building your workflow</p>
+    </div>
+  );
+}
