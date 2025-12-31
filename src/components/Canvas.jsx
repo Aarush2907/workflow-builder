85dea41dff73b58874438219489b001b4bea289d
Aarush2907 <aarush.taluja1@gmail.com>
2025-12-31 06:02:35 +0530

    refactor: render workflow using recursive renderer

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,17 +1,19 @@
 import { useWorkflow } from "../hooks/useWorkflow";
-import Node from "./Node/Node";
+import RenderNode from "./RenderNode";
 import "../styles/canvas.css";
 
 export default function Canvas() {
   const { state } = useWorkflow();
-  const startNode = state.nodes[state.rootId];
 
   return (
     <div className="canvas">
       <h1>Workflow Builder</h1>
 
       <div className="canvas-content">
-        <Node label={startNode.label} type={startNode.type} />
+        <RenderNode
+          nodeId={state.rootId}
+          nodes={state.nodes}
+        />
       </div>
     </div>
   );
