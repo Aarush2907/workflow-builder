a5a1171e6c6b2e86ddcdf20c89f7bc93ee2dbf1c
Aarush2907 <aarush.taluja1@gmail.com>
2025-12-31 06:12:29 +0530

    feat: add reducer logic to insert action node

--- src/hooks/useWorkflow.js
+++ src/hooks/useWorkflow.js
@@ -15,6 +15,30 @@ const initialState = {
 
 function workflowReducer(state, action) {
   switch (action.type) {
+    case "ADD_ACTION_NODE": {
+        const { parentId } = action.payload;
+        const newNodeId = generateNodeId("action");
+
+        return {
+        ...state,
+        nodes: {
+          ...state.nodes,
+          [newNodeId]: {
+            id: newNodeId,
+            type: "action",
+            label: "New Action",
+            next: []
+          },
+          [parentId]: {
+            ...state.nodes[parentId],
+            next: [newNodeId]
+          }
+        }
+      };
+    }
+
+
+    
     default:
       return state;
   }
